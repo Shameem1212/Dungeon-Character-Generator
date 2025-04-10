d38e2eaf97f3794f41ac87ca7ee1cbd096750480
HPoyfair <haydenpoyfair@gmail.com>
2025-04-09 23:45:03 -0600

    fix?

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
     port: 3000,
     open: true,
     proxy: {
-       // We have to set up our Vite client-side development server to proxy API requests to our server-side Node server at port 3001 in order to hit our GraphQL application.
       '/graphql': {
         target: 'http://localhost:3001',
         changeOrigin: true,
@@ -14,4 +14,8 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    outDir: 'dist',
+  },
+  base: '/', // ← this is key for routing in Render
 });
